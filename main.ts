bd10f4c6b18b968bea1e304d6bad9bd944cdc1bd
1711005 <104809338+1711005@users.noreply.github.com>
2022-06-08 10:55:52 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,7 +10,18 @@ basic.forever(function () {
     DigitalPin.P2,
     PingUnit.Centimeters
     ))
-})
-basic.forever(function () {
-    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+    if (sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    ) > 50) {
+        music.playMelody("C G E B G A F B ", 120)
+        music.playMelody("E C5 F C A A D B ", 120)
+        basic.showString("a")
+    } else {
+        basic.showIcon(IconNames.No)
+        music.playTone(440, music.beat(BeatFraction.Half))
+        music.playTone(587, music.beat(BeatFraction.Whole))
+        basic.showString("b")
+    }
 })
